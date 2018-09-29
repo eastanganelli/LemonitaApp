a72f13d1b2216231781e0b569d8394eee9f147b8
Battousai <ezequielstanganelli@gmail.com>
2018-09-29 14:20:38 -0300

    PofBattousai

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,78 +6,59 @@ import { defaultLanguage } from '../../const/languanges/languages.constant';
 
 @Component({selector:'page-home', templateUrl:'home.html'})
 export class HomePage{
-	/*HtmlVal*/
-		outPropina:number=0;
-		outBypeople:number=0;
-		outIdiom:string=null;
-		valQuality:string=null;
-		dtQuality:number=0;
-		rangeValue:number=50;
-	/*Vals*/
-	constructor(public navCtrl: NavController, public alertCtrl: AlertController, public toastCtrl: ToastController, public translate: TranslateService){
-		this.iniTrans();
-	}
+	//#region Vars
+		outPropina: number = 0;
+		outBypeople: number = 0;
+		outIdiom: string = null;
+		valQuality: string = null;
+		dtQuality: number = 0;
+		rangeValue: number = 50;
+	//#endregion
+	constructor(public navCtrl: NavController, public alertCtrl: AlertController, public toastCtrl: ToastController, public translate: TranslateService){ this.iniTrans(); }
 	/*Fns*/
 		/*MsgControllers*/
 			showAlert(vTitle:string, vSubtitle:string, vButton:string){
-		   	let alert = this.alertCtrl.create({
-		      	title: vTitle,
-		      	subTitle: vSubtitle+'.',
-		      	buttons: [vButton]
-		   	});
+		   	let alert = this.alertCtrl.create({ title: vTitle, subTitle: vSubtitle + '.', buttons: [vButton] });
 		   	alert.present();
 		  	}
 		  	showToaster(vString:string){
-		  		var toaster=this.toastCtrl.create({
-		  			message: vString,
-		  			position: 'bottom',
-		  			duration: 2500
-		  		});
+		  		let toaster = this.toastCtrl.create({ message: vString, position: 'bottom', duration: 2500 }); 
 		  		toaster.present();
 		  	}
-		/*Ini*/
-		  	iniTrans(){
-			  	if(this.translate.getBrowserLang()!==undefined){
-					let deviceLang=this.translate.getBrowserLang();
-						if(deviceLang==='ca'||deviceLang==='en'||deviceLang==='es'||deviceLang==='de'||deviceLang==='pt'||deviceLang==='fr'||deviceLang==='it')
-			      		this.translate.use(this.translate.getBrowserLang());
-			      	else
-			      		this.translate.use(defaultLanguage);
-				}else{
-				  	this.translate.setDefaultLang(defaultLanguage);
-				   this.translate.use(defaultLanguage);
-			   }
-		  	}
+		//#region Ini
+		iniTrans(){
+			if(this.translate.getBrowserLang()!==undefined){
+			 	let deviceLang = this.translate.getBrowserLang();
+				if(deviceLang === 'ca' || deviceLang === 'en' || deviceLang === 'es' || deviceLang === 'de' || deviceLang === 'pt' || deviceLang === 'fr' || deviceLang === 'it') this.translate.use(this.translate.getBrowserLang());
+				else this.translate.use(defaultLanguage);
+			} else {
+				this.translate.setDefaultLang(defaultLanguage);
+				this.translate.use(defaultLanguage);
+			}
+		}
+		//#endregion
 		mathProp(iMonto:number, iCant:number, iRange:number, iSelec:number){
+			//#region Vars
 			let varPropina: number;
 			let varBypeople:number;
-			if((iMonto<1 && String(iMonto)!="" )||( iCant<1 && String(iCant)!="")){
-				this.translate.get('TST.MSGMISS').subscribe((trText:string)=>{
-					this.showToaster(trText);
+			//#endregion
+			if((iMonto < 1 && String(iMonto) != "" ) || (iCant < 1 && String(iCant) != "")) { this.translate.get('TST.MSGMISS').subscribe((trText:string)=>{ this.showToaster(trText); }); } 
+			else if (String(iMonto) == "" || String(iCant) == "" || iSelec == null){
+				this.translate.get('ALRT.TTMISS').subscribe((trTitle:string) => {
+					this.translate.get('ALRT.MSGMISS').subscribe((trMsg:string) => {
+						this.translate.get('ALRT.BTNCLOSE').subscribe((trBtn:string) => { this.showAlert(trTitle,trMsg,trBtn); });
+					});
 				});
-			}else if(String(iMonto)=="" || String(iCant)=="" || iSelec==null){
-				this.translate.get('ALRT.TTMISS').subscribe((trTitle:string)=>{
-					this.translate.get('ALRT.MSGMISS').subscribe((trMsg:string)=>{
-						this.translate.get('ALRT.BTNCLOSE').subscribe((trBtn:string)=>{
-							this.showAlert(trTitle,trMsg,trBtn);
-						})
-					})
-				})
-			}else{
-				let percentData=iRange;
-				if(iSelec!=-1){percentData=iSelec;}
-				varPropina=iMonto/100*percentData;
-				varBypeople=varPropina/iCant;
-				/*Output Data*/
-					this.outPropina=varPropina;
-					this.outBypeople=varBypeople;
+			} else {
+				let percentData = iRange;
+				if(iSelec != (-1)) { percentData = iSelec; }
+				varPropina = iMonto/100*percentData;
+				varBypeople = varPropina/iCant;
+				//#region Output Data
+					this.outPropina = varPropina;
+					this.outBypeople = varBypeople;
+				//#endregion
 			}
 		}
-		CheckCant(vCant:number){
-			if(vCant<1 && String(vCant)!=""){
-				this.translate.get('TST.MSGMISS').subscribe((trText:string)=>{
-					this.showToaster(trText);
-				});
-			}
-		}
+		CheckCant(vCant:number){ if(vCant < 1 && String(vCant) != ""){ this.translate.get('TST.MSGMISS').subscribe((trText:string)=>{ this.showToaster(trText); }); } }
 }
--- src/pages/newmode/newmode.ts
+++ src/pages/newmode/newmode.ts
@@ -3,45 +3,115 @@ import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 @Component({ selector: 'page-newmode', templateUrl: 'newmode.html', })
 export class NewModePage {
-	ppArr: Array<{ name_: string; data: Array<{ name: string; price: number; }> ; }> = new Array(0);
-	outPropina: number = 0;
-	ppTT: number = 0;
-	constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
-		this.ppArr.push({name_: 'Me', data: new Array(0)});
-	}
-	addPer() {
-		let userpop = this.alertCtrl.create({
-			title: 'Ingresar Nombre',
-			inputs: [{ name: 'nameIN', type: 'text'}],
-			buttons: [
-				{ text: 'Cancel' },
-				{ text: 'Add', role: 'add', handler: data => {this.ppArr.push({name_: data.nameIN, data: new Array(0)}); } }
-			]
-		});
-		userpop.present();
-	}
-	openPer(id: number) {
-		let addpop = this.alertCtrl.create({
-			title: 'Consumo',
-			inputs: [{ name: 'itemIN', placeholder: 'Pedido', type: 'text' }, { name: 'precioIN', placeholder: 'Precio', type: 'number' }],
-			buttons: [
-				{ text: 'Cancel' },
-				{ text: 'Add', role: 'add', handler: data => {this.ppArr[id].data.push({name: data.itemIN, price: data.precioIN});} }
-			]
-		}); addpop.present();
-	}
-	viewPer(id: number) {
-		let viewpop = this.alertCtrl.create({title: this.ppArr[id].name_, buttons: [{ text: 'Cancel' }]}); 
-			let i = 0; for(let item_ of this.ppArr[id].data) { viewpop.addInput({ type: 'checkbox', label: item_.name + ' $' + item_.price, value: i.toString(), checked: false}); i++; }
-		if(this.ppArr[id].data.length > 0) { 
-			viewpop.addButton({ 
-				text: 'Remove', role: 'rm', handler: data => { 
-					if(data.length > 0) { for(let i = data.length; i > 0; i--) { this.ppArr[id].data.splice(data, 1); } } 
+	//#region Vars
+		//#region Arrays
+			ppArr: Array<{ name_: string; data: Array<{ name: string; price: number; }>; }> = new Array(0);
+			ppParARR: Array<{ name: string; amount: number; tip: number; total: number; }> = new Array(0);
+			ppEqARR: Array<{ name: string; total: number; }> = new Array(0);
+		//#endregion
+		//#region DataIN
+			notCalculated: boolean = true;
+			ppTT: number = 0;
+			valQuality: string = null;
+			rangeValue: number = 50;
+			tipData_: string = 'Equal_';
+		//#endregion
+		//#region DataOUT
+			equalData: { totalAmount: number; totalTip: number; totalTipDiv: number; total_: number; };
+			parcialData: { totalAmount: number; totalTip: number; totalTipEq: number; total_: number; total_EqTIP: number; };
+		//#endregion
+	//#endregion
+	constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) { this.ppArr.push({name_: 'Me', data: new Array(0)}); }
+	//#region FNs ppl
+		addPer() {
+			let userpop = this.alertCtrl.create({
+				title: 'Ingresar Nombre',
+				inputs: [{ name: 'nameIN', type: 'text'}],
+				buttons: [ { text: 'Cancel' }, { text: 'Add', role: 'add', handler: data => {this.ppArr.push({name_: data.nameIN, data: new Array(0)}); } } ]
+			}); userpop.present();
+		}
+		openPer(id: number) {
+			let addpop = this.alertCtrl.create({
+				title: 'Consumo',
+				inputs: [{ name: 'itemIN', placeholder: 'Pedido', type: 'text' }, { name: 'precioIN', placeholder: 'Precio', type: 'number' }],
+				buttons: [ { text: 'Cancel' }, { text: 'Add', role: 'add', handler: data => {this.ppArr[id].data.push({name: data.itemIN, price: data.precioIN});} } ]
+			}); addpop.present();
+		}
+		viewPer(id: number) {
+			let viewpop = this.alertCtrl.create({title: this.ppArr[id].name_, buttons: [{ text: 'Cancel' }]}); 
+				let i = 0; for(let item_ of this.ppArr[id].data) { viewpop.addInput({ type: 'checkbox', label: item_.name + ' $' + item_.price, value: i.toString(), checked: false}); i++; }
+			if(this.ppArr[id].data.length > 0) { 
+				viewpop.addButton({ 
+					text: 'Remove', role: 'rm', handler: data => { if(data.length > 0) { for(let i = data.length; i > 0; i--) { this.ppArr[id].data.splice(data, 1); } }  }
+				});
+			} viewpop.present();
+		}
+		rmPer(id: number) { this.ppArr.splice(id, 1); }
+		notMe(id: number) { if(id != 0) { return false; } return true; }
+	//#endregion
+	calculate(iRange: number, iSelec: number) {
+		console.log(this.itsComplete(iSelec).length);
+		if(this.itsComplete(iSelec).length < 1) {
+			this.notCalculated = false;
+			let percentData: number = iRange; let ppCount_: number = 0;
+			if(iSelec != (-1)) { percentData = iSelec; }
+			let allAmount: number = 0/*, eqTIP: number = 0*/, parTIP: number = 0/*, eqTotal: number = 0, parTotal: number = 0*/;
+			for(let pp_ of this.ppArr) {
+				if(pp_.data.length > 0) {
+					ppCount_++;
+					//#region Vars
+					let ppParARR: { name: string; amount: number; tip: number; total: number; } = { name: pp_.name_, amount: 0, tip: 0, total: 0 };
+					let ppEqARR: { name: string; total: number; } = { name: pp_.name_, total: 0 };
+					let amount_: number = 0;
+					//#endregion
+					for(let csmp_ of pp_.data) { amount_ = Number(Number(amount_) + Number(csmp_.price)); }
+					//#region Data Record
+						//#region Parcial
+							ppParARR.amount = amount_; 
+							ppParARR.tip = Number(amount_/100*percentData); 
+							ppParARR.total = (Number(amount_) + Number(ppParARR.tip));
+						//Total Parcial
+							parTIP = Number(Number(parTIP) + Number(amount_/100*percentData));
+						//#endregion
+						//Equal
+							ppEqARR.total = Number(amount_);
+						//For All
+							allAmount = Number(Number(allAmount) + Number(amount_));
+					//#endregion
+					//#region Push data
+						this.ppParARR.push(ppParARR);
+						this.ppEqARR.push(ppEqARR);
+					//#endregion
 				}
-			});
-		} 
-		viewpop.present();
+			} 
+			//#region Equal Total
+				this.equalData = { 
+					totalAmount: allAmount, 
+					totalTip: Number(allAmount/percentData), 
+					totalTipDiv: ((allAmount/percentData)/ppCount_), 
+					total_: (Number(allAmount) + Number(allAmount/percentData)) 
+				};
+			//#endregion
+			//#region ParTotal
+				this.parcialData = { 
+					totalAmount: allAmount, 
+					totalTip: parTIP, 
+					total_: (allAmount + Number((allAmount*percentData)/ppCount_)), 
+					totalTipEq: ((allAmount*percentData)/ppCount_), 
+					total_EqTIP: (Number(allAmount) + Number(allAmount/percentData))
+				};
+			//#endregion
+		} //else { alert('Falta data'); }
 	}
-	rmPer(id: number) { this.ppArr.splice(id, 1); }
-	notMe(id: number) { if(id != 0) { return false; } return true; }
+	itsComplete(iSelec: number): Array<{ code_: string; }> {
+		let errArr: Array<{ code_: string; }> = new Array(0);
+		if(iSelec == null) { errArr.push({ code_: '1xQuality' }); }
+		if(this.ppArr[0].data.length < 1 && this.ppArr.length < 2) { errArr.push({ code_: '1xNoMeData' }); }
+		return errArr;
+	}
+	close() { 
+		this.notCalculated = true;
+		this.ppEqARR = new Array(0);
+		this.ppParARR = new Array(0);
+	}
 }
