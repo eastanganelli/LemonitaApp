fafbf5a25c57c5d22bf59cc0acd54f94c6760934
Ezequiel A Stanganelli <ezequielstanganelli@gmail.com>
2019-07-24 01:12:29 -0300

    Zequi <DP CEO>

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -3,64 +3,64 @@ import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
-  {
-    path: 'tabs',
-    component: TabsPage,
-    children: [
-      {
-        path: 'tab1',
-        children: [
-          {
-            path: '',
-            loadChildren: '../home/home.module#HomePageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab2',
-        children: [
-          {
-            path: '',
-            loadChildren: '../advance/advance.module#AdvancePageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: '../history/history.module#HistoryPageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: '../settings/settings.module#SettingsPageModule'
-          }
-        ]
-      },
-      {
-        path: '',
-        redirectTo: '/home/home',
-        pathMatch: 'full'
-      }
-    ]
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
+	{
+		path: 'tabs',
+		component: TabsPage,
+		children: [
+			{
+				path: 'tab1',
+				children: [
+					{
+						path: '',
+						loadChildren: '../home/home.module#HomePageModule'
+					}
+				]
+			},
+			{
+				path: 'tab2',
+				children: [
+					{
+						path: '',
+						loadChildren: '../advance/advance.module#AdvancePageModule'
+					}
+				]
+			},
+			{
+				path: 'tab3',
+				children: [
+					{
+						path: '',
+						loadChildren: '../history/history.module#HistoryPageModule'
+					}
+				]
+			},
+			{
+				path: 'tab4',
+				children: [
+					{
+						path: '',
+						loadChildren: '../settings/settings.module#SettingsPageModule'
+					}
+				]
+			},
+			{
+				path: '',
+				redirectTo: '/home/home',
+				pathMatch: 'full'
+			}
+		]
+	},
+	{
+		path: '',
+		redirectTo: '/tabs/tab1',
+		pathMatch: 'full'
+	}
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forChild(routes)
-  ],
-  exports: [RouterModule]
+	imports: [
+		RouterModule.forChild(routes)
+	],
+	exports: [RouterModule]
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule { }
