886d42a61d3a677fd706430d1e4a6cc628dd529c
Ezequiel A. Stanganelli <ezequielstanganelli@gmail.com>
2024-11-14 21:16:32 -0300

    Changes for admob

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,27 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/services.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:tip_calculator/shared_data.dart';
 import 'package:tip_calculator/amount_widget.dart';
 import 'package:tip_calculator/people_widget.dart';
 import 'package:tip_calculator/tipping_widget.dart';
 import 'package:tip_calculator/total_widget.dart';
 
-void main() {
-  /*WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((value) => {
-            runApp(
-              ChangeNotifierProvider(
-                create: (context) => SharedData(),
-                child: const MyApp(),
-              ),
-            )
-          });*/
-  runApp(
-    /// Providers are above [MyApp] instead of inside it, so that tests
-    /// can use [MyApp] while mocking the providers
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => SharedData()),
-      ],
-      child: const MyApp(),
-    ),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await MobileAds.instance.initialize();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (value) => {
+      runApp(
+        MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => SharedData()),
+          ],
+          child: const MyApp(),
+        ),
+      )
+    },
   );
 }
 
@@ -46,16 +41,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatefulWidget {
   final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isBannerLoaded = false;
+  late BannerAd bannerAd;
+
+  inilizeBannerAd() async {
+    bannerAd = BannerAd(
+      size: AdSize.banner,
+      adUnitId: 'ca-app-pub-2237199373273098/8606613360',
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            isBannerLoaded = true;
+            print("Banner has been loaded!");
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+          isBannerLoaded = false;
+          print(error);
+        },
+      ),
+      request: const AdRequest(),
+    );
+
+    bannerAd.load();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    inilizeBannerAd();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: SafeArea(
         child: Column(
@@ -84,7 +116,7 @@ class MyHomePage extends StatelessWidget {
                 ],
               ),
             ),
-            Expanded(
+            const Expanded(
               //flex: 1,
               child: MyTotalWidget(),
             ),
